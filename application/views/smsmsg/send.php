8a117f7cfa2ff465efc47e17296e8c263e9ec302
Andrew Li <andrew.lidev@yandex.com>
2018-07-30 12:10:22 +0200

    send page

--- application/views/smsmsg/send.php
+++ application/views/smsmsg/send.php
@@ -16,9 +16,7 @@
         ?>
 
         <div class="col-md-offset-1 col-md-10">
-	        	<?php
-	        		echo form_open('smsmsg/callsendsms');
-	        	?>
+          <form method="POST" action="/smsmsg/callsendsms">
             <h4>Sms Reply.</h4>
             <input type="text" id="phonenumber" class="form-control input-sm chat-input" placeholder="phonenumber" name="phonenum" value="<?php echo($phonenum); ?>"> 
             </br>
@@ -33,9 +31,7 @@
                 <button type="submit" class="btn btn-primary btn-md">send <i class="fa fa-sign-in"></i></a>
             </span>
             </div>
-	        	<?php
-	        		echo form_close();
-	        	?>
+          </form>
         
         </div>
     </div>
